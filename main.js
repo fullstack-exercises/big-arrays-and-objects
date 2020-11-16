1b3fdbd0df5026c45081998de15bd95c703e201c
Jessica Michel <jessicamichel93@gmail.com>
2020-11-16 15:32:58 +0100

    resersed old code and made it work, sending jokes from api to DOM

--- main.js
+++ main.js
@@ -1,22 +1,37 @@
 const container = document.querySelector('.container');
 
-const fetchData = async(endpoint) => {
-    try {
-        const API_URL = `https://icanhazdadjoke.com${endpoint}`;
-        const options = {
-            method: "GET",
-            headers: { "Accept": "application/json" } // fixes “No Access-Control-Allow-Origin header”
-        };
-        let getJoke = await fetch(API_URL, options);
-        console.log(getJoke);
-        let jokeDiv = document.createElement('div');
-        jokeDiv.classList.add('joke');
-        container.appendChild(jokeDiv);
-        let jokeResults = document.querySelector('.joke');
-        return getJoke;
-
-    } catch (error) {
-        console.log(error);
-    }
+const getRandomJoke = (endpoint) => {
+    const API_URL = `https://icanhazdadjoke.com${endpoint}`;
+    const options = {
+        method: "GET",
+        headers: { "Accept": "application/json" } // fixes “No Access-Control-Allow-Origin header”
+    };
+    fetch(API_URL, options)
+        .then(response => response.json())
+        .then(data => {
+            let jokeResults = document.querySelector('.joke');
+            jokeResults.innerHTML = data.joke;
+            // console.log(data.joke);
+        })
+        .catch(error => {
+            console.log(error);
+        });
 }
-fetchData('');
+
+const createJoke = async() => {
+    let getJoke = await getRandomJoke('');
+    let jokeDiv = document.createElement('div');
+    jokeDiv.classList.add('joke');
+    container.appendChild(jokeDiv);
+
+    return getJoke;
+}
+createJoke();
+
+// Requirements:
+
+// Als gebruiker wil ik bij het landen op de pagina uitgenodigd worden om op een knop te klikken.
+// Als gebruiker wil ik na het klikken op de knop 1 grap, tweet of pokemon te zien krijgen.
+// Bekijk nog minstens 1 andere feature van de API en zorg dat jouw gebruiker hier gebruik van kan maken (wees creatief!) :).
+// Bijvoorbeeld: DadJokes API kan ook plaatjes van dadjokes ophalen ipv tekst.
+// PokemonAPI: kan ook de strengths en weaknesses van de pokemon tonen.
