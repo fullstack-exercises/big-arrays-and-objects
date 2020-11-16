d0c7a92461446bde65a6f801379c692f35838264
Jessica Michel <jessicamichel93@gmail.com>
2020-11-16 16:11:35 +0100

    finetuning plus added .jokes div inside html instead JS

--- main.js
+++ main.js
@@ -1,37 +1,32 @@
 const container = document.querySelector('.container');
+const generateJokeButton = document.querySelector('.button');
 
-const getRandomJoke = (endpoint) => {
-    const API_URL = `https://icanhazdadjoke.com${endpoint}`;
-    const options = {
-        method: "GET",
-        headers: { "Accept": "application/json" } // fixes “No Access-Control-Allow-Origin header”
-    };
-    fetch(API_URL, options)
-        .then(response => response.json())
-        .then(data => {
-            let jokeResults = document.querySelector('.joke');
-            jokeResults.innerHTML = data.joke;
-            // console.log(data.joke);
-        })
-        .catch(error => {
-            console.log(error);
-        });
+const fetchData = (endpoint) => {
+    try {
+        const API_URL = `https://icanhazdadjoke.com${endpoint}`;
+        const options = {
+            method: "GET",
+            headers: { "Accept": "application/json" } // fixes “No Access-Control-Allow-Origin header”
+        };
+        fetch(API_URL, options)
+            .then(response => response.json())
+            .then(data => {
+                let jokeResults = document.querySelector('.joke');
+                jokeResults.innerHTML = data.joke;
+            })
+
+    } catch (error) {
+        console.log(error);
+    }
 }
 
-const createJoke = async() => {
-    let getJoke = await getRandomJoke('');
-    let jokeDiv = document.createElement('div');
-    jokeDiv.classList.add('joke');
-    container.appendChild(jokeDiv);
-
+const fetchRandomJoke = async() => {
+    let getJoke = await fetchData('');
     return getJoke;
 }
-createJoke();
+fetchRandomJoke();
 
-// Requirements:
 
-// Als gebruiker wil ik bij het landen op de pagina uitgenodigd worden om op een knop te klikken.
-// Als gebruiker wil ik na het klikken op de knop 1 grap, tweet of pokemon te zien krijgen.
-// Bekijk nog minstens 1 andere feature van de API en zorg dat jouw gebruiker hier gebruik van kan maken (wees creatief!) :).
-// Bijvoorbeeld: DadJokes API kan ook plaatjes van dadjokes ophalen ipv tekst.
-// PokemonAPI: kan ook de strengths en weaknesses van de pokemon tonen.
+generateJokeButton.addEventListener('click', () => {
+    fetchRandomJoke();
+});
